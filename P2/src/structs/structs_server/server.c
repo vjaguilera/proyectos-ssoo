29c9841a31dfee0b40cce56e2cd551c981045627
nachocontreras <icontreras1@uc.cl>
2021-06-08 12:32:38 -0400

    Socket lider

--- P2/src/structs/structs_server/server.c
+++ P2/src/structs/structs_server/server.c
@@ -19,70 +19,111 @@ void initial_listen(Server* server) {
   int pos;
   int msg_code;
   char response[50];
-  int espera_cliente = 0;
-  int not_start = 1;
-
+  pthread_t tid[1];
+  ArgumentsCreateThread* thrgs = malloc(sizeof(ArgumentsCreateThread));
+  thrgs -> not_start = 1;
+  
   char * welcome = "¡Bienvenido Cliente!\nEscriba su nombre:";
   Jugador* actual;
-  while (1) {
+  while (thrgs -> not_start) {
     // Se definen las estructuras para almacenar info sobre los sockets de los clientes
     struct sockaddr_in client1_addr;
     socklen_t addr_size = sizeof(client1_addr);
-    new_socket = accept(server -> socket, (struct sockaddr *)&client1_addr, &addr_size);
-    espera_cliente = 1;
-    printf("Enviamos mensaje bienvenida a %d\n", new_socket);
-    pos = server -> cantidad_clientes;
-    server -> clientes[server -> cantidad_clientes] = init_jugador();
-    actual = server -> clientes[server -> cantidad_clientes];
-    set_socket(actual, new_socket);
+
+    fd_set set;
+    struct timeval timeout;
+    int rv;
+    FD_ZERO(&set); /* clear the set */
+    FD_SET(server -> socket, &set); /* add our file descriptor to the set */
+
+    timeout.tv_sec = 2;
+    timeout.tv_usec = 0;
+
+    rv = select(server -> socket + 1, &set, NULL, NULL, &timeout);
+    if (rv == -1)
+    {
+        perror("select"); /* an error occurred */
+    }
+    else if (rv == 0)
+    {
+      // printf("timeout occurred (20 second) \n"); /* a timeout occurred */
+    }
+    else
+    {
+      new_socket = accept(server -> socket, (struct sockaddr *)&client1_addr, &addr_size);
     
-    // ask for name
-    printf("Mandar a %d\n", actual -> socket);
-    server_send_message(actual -> socket, 1, welcome);
-    msg_code = server_receive_id(new_socket);
-    if (msg_code == 1) {
-      char * client_message = server_receive_payload(new_socket);
-      printf("El cliente %d dice: %s\n", server -> cantidad_clientes, client_message);
-      actual -> nombre = client_message;
 
-    }
-    server_send_message(new_socket, 2, "Seleccione su clase");
-    msg_code = server_receive_id(new_socket);
-    if (msg_code == 1) {
-      char * client_message = server_receive_payload(new_socket);
-      printf("El cliente %d dice: %s\n", server -> cantidad_clientes, client_message);
-      actual -> num_clase = atoi(client_message);
-    }
-    espera_cliente = 0;
+      thrgs -> espera_cliente = 1;
+      printf("Enviamos mensaje bienvenida a %d\n", new_socket);
+      pos = server -> cantidad_clientes;
+      server -> clientes[server -> cantidad_clientes] = init_jugador();
+      actual = server -> clientes[server -> cantidad_clientes];
+      set_socket(actual, new_socket);
+      
+      // ask for name
+      printf("Mandar a %d\n", actual -> socket);
+      server_send_message(actual -> socket, 1, welcome);
+      msg_code = server_receive_id(new_socket);
+      if (msg_code == 1) {
+        char * client_message = server_receive_payload(new_socket);
+        printf("El cliente %d dice: %s\n", server -> cantidad_clientes, client_message);
+        actual -> nombre = client_message;
 
-    server -> cantidad_clientes += 1;
+      }
+      server_send_message(new_socket, 2, "Seleccione su clase");
+      msg_code = server_receive_id(new_socket);
+      if (msg_code == 1) {
+        char * client_message = server_receive_payload(new_socket);
+        printf("El cliente %d dice: %s\n", server -> cantidad_clientes, client_message);
+        actual -> num_clase = atoi(client_message);
+      }
+      thrgs -> espera_cliente = 0;
 
-    // quiero que esto sea en paralelo
-    if (pos == 0) {
-      server -> lider = actual;
-      while (not_start) {
-        server_send_message(new_socket, 4, "¿Quiere comenzar?");
-        msg_code = server_receive_id(new_socket);
-        if (msg_code == 1) {
-          char * client_message = server_receive_payload(new_socket);
-          printf("El cliente %d dice: %s\n", server -> cantidad_clientes, client_message);
-          if (espera_cliente == 1) {
-              server_send_message(new_socket, 4, "Hay un cliente registrándose.\n¿Quiere comenzar?");
-          } else if (atoi(client_message) == 1) {
-            not_start = 0;
-            printf("Empezará la partida\n");
-            notify_all_clients(server, "¡Comenzará la partida!");
-          }
+      server -> cantidad_clientes += 1;
+
+      // quiero que esto sea en paralelo
+      if (pos == 0) {
+        server -> lider = actual;
+        thrgs -> socket = new_socket;
+        if ( pthread_create(&tid[0], NULL, leader_start, thrgs) != 0 ) {
+          printf("Failed to create thread\n");
         }
       }
+
+      sprintf(response, "%s ha ingresado a la partida con la clase %d\n", actual -> nombre, actual -> num_clase);
+      notify_all_clients(server, response);
     }
 
-    sprintf(response, "%s ha ingresado a la partida con la clase %d\n", actual -> nombre, actual -> num_clase);
-    notify_all_clients(server, response);
-
-    if (server -> cantidad_clientes == 3) {
+    if (thrgs -> not_start == 0) {
       printf("Termina la elección de clientes\n");
       break;
     }
   }
+  printf("Empezará la partida\n");
+  notify_all_clients(server, "¡Comenzará la partida!");
+}
+
+void * leader_start(void *args) {
+  int msg_code;
+  ArgumentsCreateThread * info = args;
+
+  int op = 1;
+  while (op ) {
+    server_send_message(info -> socket, 4, "¿Quiere comenzar?");
+    msg_code = server_receive_id(info -> socket);
+    if (msg_code == 1) {
+      char * client_message = server_receive_payload(info -> socket);
+      printf("El cliente lider dice: %s\n", client_message);
+      if (info -> espera_cliente == 1) {
+          server_send_message(info -> socket, 4, "Hay un cliente registrándose.\n¿Quiere comenzar?");
+      } else if (atoi(client_message) == 1) {
+        op = 0;
+        info -> not_start = 0;
+        break;
+      } else {
+      }
+    }
+  }
+  printf("Termine la función del lider\n");
+  pthread_exit(NULL);
 }
--- P2/src/structs/structs_server/server.h
+++ P2/src/structs/structs_server/server.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <pthread.h>
 #include "../structs_shared/jugador.h"
 #include "../structs_server/comunication.h"
 
@@ -9,8 +10,16 @@ typedef struct server{
     int socket;
 } Server;
 
+typedef struct thread_arguments {
+  int socket;
+  int not_start;
+  int espera_cliente;
+} ArgumentsCreateThread;
+
 Server* init_server(int socket);
 
 void notify_all_clients(Server* server, char* msg);
 
 void initial_listen(Server* server);
+
+void* leader_start(void *args);
